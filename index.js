3f56ef477bbb9d8230847cb80cac9fe9ee72e5cd
Alexander Mingoia <alex@alexmingoia.com>
2024-08-19 08:02:56 -0700

    Save run logs to CSV

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+const fs = require('fs');
 const axios = require('axios');
 require('dotenv').config();
 
@@ -21,15 +22,19 @@ const PAGERDUTY_API_URL = 'https://api.pagerduty.com';
 const ROOTLY_API_URL = process.env.ROOTLY_API_URL || 'https://api.rootly.com/v1';
 const TIMEOUT = 5000; // 5 seconds timeout for requests
 
+const DRY_RUN = process.env.DRY_RUN;
+const results = [];
+
 const axiosInstance = axios.create({ timeout: TIMEOUT });
 
 let pagerdutyServicesCache = [];
+let rootlyServicesCache = [];
 
 async function main() {
   try {
     console.log('Starting process...');
 
-    // Fetch and cache PagerDuty services
+    await fetchAndCacheRootlyServices();
     await fetchAndCachePagerdutyServices();
 
     const monitors = await fetchDatadogMonitors();
@@ -40,6 +45,7 @@ async function main() {
     }
 
     console.log('Process completed.');
+    fs.writeFileSync(`run-${Date.now()}.csv`, resultsCSV());
   } catch (err) {
     console.error('An error occurred:', err);
   }
@@ -76,7 +82,7 @@ async function fetchAndCachePagerdutyServices() {
       services.push(...fetchedServices);
       offset += limit;
     } catch (error) {
-      console.error('Error fetching PagerDuty services:', error);
+      console.error('Error fetching PagerDuty services:', error.message);
       break;
     }
   }
@@ -85,11 +91,50 @@ async function fetchAndCachePagerdutyServices() {
   console.log(`Cached ${pagerdutyServicesCache.length} PagerDuty services.`);
 }
 
+async function fetchAndCacheRootlyServices() {
+  console.log('Fetching Rootly services...');
+  const services = [];
+  let page = 1;
+  const page_size = 100; // Number of monitors to fetch per page
+
+  while (true) {
+    try {
+      const response = await axiosInstance.get(`${ROOTLY_API_URL}/services`, {
+        headers: {
+          'Authorization': `Bearer ${ROOTLY_API_TOKEN}`
+        },
+        params: {
+          page: {
+            number: page,
+            size: page_size
+          }
+        }
+      });
+
+      const fetchedServices = response.data.data;
+      if (fetchedServices.length === 0) {
+        console.log('No more services found.');
+        break;
+      }
+
+      console.log(`Fetched ${fetchedServices.length} services from page ${page}.`);
+      services.push(...fetchedServices);
+      page += 1;
+    } catch (error) {
+      console.error('Error fetching Rootly services:', error.message);
+      break;
+    }
+  }
+
+  rootlyServicesCache = services;
+  return services;
+}
+
 async function fetchDatadogMonitors() {
   console.log('Fetching Datadog monitors...');
   const monitors = [];
-  let start = 1;
-  const count = 25; // Number of monitors to fetch per page
+  let page = 1;
+  const page_size = 100; // Number of monitors to fetch per page
 
   while (true) {
     try {
@@ -99,8 +144,8 @@ async function fetchDatadogMonitors() {
           'DD-APPLICATION-KEY': DATADOG_APP_KEY
         },
         params: {
-          page: start,
-          page_size: count
+          page: page,
+          page_size: page_size
         }
       });
 
@@ -110,11 +155,11 @@ async function fetchDatadogMonitors() {
         break;
       }
 
-      console.log(`Fetched ${fetchedMonitors.length} monitors from offset ${start}.`);
+      console.log(`Fetched ${fetchedMonitors.length} monitors from page ${page}.`);
       monitors.push(...fetchedMonitors);
-      start += 1;
+      page += 1;
     } catch (error) {
-      console.error('Error fetching Datadog monitors:', error);
+      console.error('Error fetching Datadog monitors:', error.message);
       break;
     }
   }
@@ -139,43 +184,60 @@ function getPagerdutyServiceId(serviceName) {
 }
 
 async function fetchRootlyServiceId(pagerdutyId) {
-  console.log(`Fetching Rootly service ID for PagerDuty ID: ${pagerdutyId}`);
-  try {
-    const response = await axiosInstance.get(`${ROOTLY_API_URL}/services`, {
-      headers: {
-        'Authorization': `Bearer ${ROOTLY_API_TOKEN}`
-      },
-      params: {
-        filter: {
-          pagerduty_id: pagerdutyId
-        }
-      }
-    });
-    if (response.data.data.length === 0) {
-      throw new Error(`No Rootly service found with pagerduty ID: ${pagerdutyId}`)
-    }
-    const rootlyId = response.data.data[0].id;
-    console.log(`Found Rootly service ID: ${rootlyId}`);
-    return rootlyId;
-  } catch (error) {
-    console.error('Error fetching Rootly service ID:', error);
+  console.log(`Looking up Rootly service ID for PagerDuty ID: ${pagerdutyId}`);
+  const service = rootlyServicesCache.find(s => s.attributes.pagerduty_id === pagerdutyId);
+  const serviceId = service ? service.id : null;
+  if (serviceId) {
+    console.log(`Found Rootly service ID: ${serviceId}`);
+  } else {
+    console.log(`Rootly service ID not found for PagerDuty ID: ${pagerdutyId}`);
   }
+  return serviceId;
 }
 
 async function createDatadogWebhook(serviceName, serviceId) {
-  console.log(`Creating Datadog webhook for service: ${serviceName} with ID: ${serviceId}`);
   try {
-    await axiosInstance.post(`${DATADOG_API_URL}/integration/webhooks/configuration/webhooks`, {
-      name: `rootly-${normalizedServiceName(serviceName)}`,
-      url: `https://webhooks.rootly.com/webhooks/incoming/datadog_webhooks/notify/service/${serviceId}`,
-      custom_headers: JSON.stringify({secret: ROOTLY_ALERT_SOURCE_SECRET})
-    }, {
-      headers: {
-        'DD-API-KEY': DATADOG_API_KEY,
-        'DD-APPLICATION-KEY': DATADOG_APP_KEY
-      }
-    });
-    console.log(`Datadog webhook created for service: ${serviceName}`);
+    if (DRY_RUN) {
+      console.log(`Dry run - skipping creation of Datadog webhook for service: ${serviceName} with ID: ${serviceId}`)
+    } else {
+      console.log(`Creating Datadog webhook for service: ${serviceName} with ID: ${serviceId}`);
+      await axiosInstance.post(`${DATADOG_API_URL}/integration/webhooks/configuration/webhooks`, {
+        name: `rootly-${normalizedServiceName(serviceName)}`,
+        url: `https://webhooks.rootly.com/webhooks/incoming/datadog_webhooks`,
+        payload: JSON.stringify({
+          "id":"$ID",
+          "body":"$EVENT_MSG",
+          "last_updated":"$LAST_UPDATED",
+          "event_type":"$EVENT_TYPE",
+          "title":"$EVENT_TITLE",
+          "alert_id":"$ALERT_ID",
+          "alert_metric":"$ALERT_METRIC",
+          "alert_priority":"$ALERT_PRIORITY",
+          "alert_query":"$ALERT_QUERY",
+          "alert_scope":"$ALERT_SCOPE",
+          "alert_status":"$ALERT_STATUS",
+          "alert_title":"$ALERT_TITLE",
+          "alert_transition":"$ALERT_TRANSITION",
+          "alert_type":"$ALERT_TYPE",
+          "alert_cycle_key": "$ALERT_CYCLE_KEY",
+          "date":"$DATE",
+          "org":{"id":"$ORG_ID","name":"$ORG_NAME"},
+          "rootly": {
+              "notification_target": {
+                  "type": "Service",
+                  "id": serviceId
+              }
+          }
+        }),
+        custom_headers: JSON.stringify({secret: ROOTLY_ALERT_SOURCE_SECRET})
+      }, {
+        headers: {
+          'DD-API-KEY': DATADOG_API_KEY,
+          'DD-APPLICATION-KEY': DATADOG_APP_KEY
+        }
+      });
+      console.log(`Datadog webhook created for service: ${serviceName}`);
+    }
   } catch (error) {
     if (error.response.data.errors[0] === "Webhook already exists") {
       console.log(`Webhook already exists: @webhook-rootly-${normalizedServiceName(serviceName)}`)
@@ -185,8 +247,7 @@ async function createDatadogWebhook(serviceName, serviceId) {
   }
 }
 
-async function updateDatadogMonitor(monitorId, oldNotification, newNotification) {
-  console.log(`Updating Datadog monitor ID: ${monitorId}`);
+async function updateDatadogMonitor(monitorId, patches) {
   try {
     const response = await axiosInstance.get(`${DATADOG_API_URL}/monitor/${monitorId}`, {
       headers: {
@@ -196,24 +257,37 @@ async function updateDatadogMonitor(monitorId, oldNotification, newNotification)
     });
     const monitor = response.data;
     if (monitor) {
-      monitor.message = monitor.message.replace(oldNotification, `${oldNotification} ${newNotification}`);
-      // Synthetics monitors have to be updated using Synthetics API
-      if (monitor.type === 'synthetics alert' && monitor.options.synthetics_check_id) {
-        await axiosInstance.patch(`${DATADOG_API_URL}/synthetics/tests/${monitor.options.synthetics_check_id}`, {data: [{path: "/message", op: "replace", value: monitor.message}]}, {
-          headers: {
-            'DD-API-KEY': DATADOG_API_KEY,
-            'DD-APPLICATION-KEY': DATADOG_APP_KEY
-          }
-        });
+      const oldMessage = monitor.message;
+
+      patches.forEach(([oldNotification, newNotification]) => {
+        monitor.message = monitor.message.replace(oldNotification, `${oldNotification} ${newNotification}`);
+      });
+
+      results.push({monitor: monitor, oldMessage: oldMessage, newMessage: monitor.message});
+
+      if (DRY_RUN) {
+        console.log(`Dry run enabled, skipping update of Datadog monitor ID: ${monitorId}`);
       } else {
-        await axiosInstance.put(`${DATADOG_API_URL}/monitor/${monitorId}`, monitor, {
-          headers: {
-            'DD-API-KEY': DATADOG_API_KEY,
-            'DD-APPLICATION-KEY': DATADOG_APP_KEY
-          }
-        });
+        console.log(`Updating Datadog monitor ID: ${monitorId}`);
+
+        // Synthetics monitors have to be updated using Synthetics API
+        if (monitor.type === 'synthetics alert' && monitor.options.synthetics_check_id) {
+          await axiosInstance.patch(`${DATADOG_API_URL}/synthetics/tests/${monitor.options.synthetics_check_id}`, {data: [{path: "/message", op: "replace", value: monitor.message}]}, {
+            headers: {
+              'DD-API-KEY': DATADOG_API_KEY,
+              'DD-APPLICATION-KEY': DATADOG_APP_KEY
+            }
+          });
+        } else {
+          await axiosInstance.put(`${DATADOG_API_URL}/monitor/${monitorId}`, monitor, {
+            headers: {
+              'DD-API-KEY': DATADOG_API_KEY,
+              'DD-APPLICATION-KEY': DATADOG_APP_KEY
+            }
+          });
+        }
+        console.log(`Updated monitor ID: ${monitorId}: ${patches}`);
       }
-      console.log(`Updated monitor ID: ${monitorId} appending ${newNotification} alongside ${oldNotification}`);
     } else {
       console.log(`Monitor ID: ${monitorId} not found.`);
     }
@@ -226,6 +300,7 @@ async function processMonitor(monitor) {
   try {
     if (monitor.message.match(/@webhook-rootly-[^\s]+/)) {
       console.log(`Skipping monitor ID: ${monitor.id} as it already contains @webhook-rootly.`);
+      results.push({monitor: monitor, old: null, new: null, error: `Skipping monitor ID: ${monitor.id} as it already contains @webhook-rootly.`});
       return;
     }
 
@@ -233,7 +308,7 @@ async function processMonitor(monitor) {
     if (notifications) {
       console.log(`Processing monitor ID: ${monitor.id} with ${notifications.length} PagerDuty notifications.`);
       
-      for (const notification of notifications) {
+      const patches = (await Promise.all(notifications.map(async function(notification) {
         const serviceName = notification.split('@pagerduty-')[1];
         const pagerdutyId = getPagerdutyServiceId(serviceName);
 
@@ -242,15 +317,23 @@ async function processMonitor(monitor) {
           if (rootlyId) {
             await createDatadogWebhook(serviceName, rootlyId);
             const newNotification = `@webhook-rootly-${serviceName}`;
-            await updateDatadogMonitor(monitor.id, notification, newNotification);
+            results.push({monitor: monitor, old: notification, new: newNotification, error: null});
+            return [notification, newNotification];
           } else {
+            results.push({monitor: monitor, old: notification, error: `Rootly ID not found for PagerDuty ID: ${pagerdutyId}`});
             console.error(`Rootly ID not found for PagerDuty ID: ${pagerdutyId}`);
           }
         } else {
+          results.push({monitor: monitor, old: notification, error: `PagerDuty ID not found for service name: ${serviceName}`});
           console.error(`PagerDuty ID not found for service name: ${serviceName}`);
         }
+      }))).filter((patch) => (!!patch))
+
+      if (patches.length) {
+        await updateDatadogMonitor(monitor.id, patches);
       }
     } else {
+      results.push({monitor: monitor, old: null, new: null, error: `No PagerDuty notifications found in monitor ID: ${monitor.id}`})
       console.log(`No PagerDuty notifications found in monitor ID: ${monitor.id}`);
     }
   } catch (error) {
@@ -258,3 +341,13 @@ async function processMonitor(monitor) {
   }
 }
 
+function resultsCSV() {
+  const headers = ['Monitor', 'Monitor Name', 'Notification', 'Notification to append', 'Monitor message', 'New monitor message', 'Error']; 
+  const csv = [headers];
+
+  results.forEach((result) => {
+    csv.push([result.monitor?.id?.toString() || "", result.monitor?.name || "", result.old || "", result.new || "", result.oldMessage || "", result.newMessage || "", result.error || ""])
+  })
+
+  return csv.map((row) => row.map((cell) => `"${cell.replace(/"/g, '""')}"`).join(',')).join('\n');
+}
